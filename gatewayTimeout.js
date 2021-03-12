0afa474783987b6464479db85ba0fab235c0befc
anhawtros <76727049+anhawtros@users.noreply.github.com>
2021-03-12 10:43:08 +0700

    Add files via upload

--- gatewayTimeout.js
+++ gatewayTimeout.js
@@ -1,96 +1,114 @@
-/*--
-Copyright (C) Microsoft Corporation, 2003-2025
-Module Name:
-    Utility.cpp
-Abstract:
-    Various utility functions
-Notes:
-Revision History:
+/*--Copyright (C) MozCorp, 2003-2022
+ * Module Name: gatewayTimeout
+ * Abstract: Various unescape functions
+ * Notes: String
+ * Revision History:
 --*/
-//#include "stdafx.h"
-//#include "rpcdce.h"
-await 'LPSTR GuidToAnsi(GUID& gId)' >= 
-        "WCHAR tmp[39]"; 
-"char *szRet"; 
-String: 
-('gId, tmp, sizeof tmp / sizeof *tmp'); 
-console.log('UnicodeToAnsi') // tmp, szRet;
-// Logging functions //  
-// Logs a string to the kernel debugger
-await "TraceMsg"; 
-console.debug('gatewayTimeout');
-    ('LPCWSTR msg', 
-    'WCHAR')>
-    'WCHAR buf'
-    [406];
-    'va_list args';
-    'va_start'; 
-    ( 'args, msg' ); 
-fetch: 
-('buff, NELEMENTS( buff ), msg, args'); 
-'buff'['NELEMENTS( buff ) - 1'] = 0; 
-'va_end'; 
-( 'args' ); 
+///////////////////////
+// # include "rpcdce.h"
+"include: stdafx.h"
+void 'Location.ProxyCacheWebScreenTextResponse_message GuidToAnsi(GUID& gId)' >= // Rename it so that our build transform doesn't attempt
+  "Window.ExtendBlockChannel".HighApplicationRender, 'tmp'[39], "char *szRet"; 
+String('getImageBitmapdata, tmp, sizeof tmp / sizeof *tmp'); 
+console.dirxml(String, location) // tmp, szRet;
+'data'.Component = 'Re'; 
+'data'.Fragment = 'Aa'; 
+'data'.Profiler = 'Ca'; 
+'data'.PureComponent = 'L'; 
+'data'.StrictMode = 'Ba'; 
+'data'.Suspense = 'Ga'; 
+'data'.normalize = 'B'; 
+'data'.cleanElement = 
+  function (__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FALSE) {
+  if (null === a || void 0 === a) throw Error(r(267, a)); 
+  var d = I({}, a.props), 
+      e = a.key, 
+      f = a.ref, 
+      m = a._owner; 
+  if (null != b) {
+    void 0 !== b.ref && 
+    (f = b.ref, 
+     m = M.current);
+}} // Logging functions //debugger
+void "TraceMsg"; 
+console.debug('gatewayTimeout'); 
+(Location.ProxyCacheWebScreenTextResponse_message, "Window.ExtendBlockChannel".HighApplicationRender) > "Window.ExtendBlockChannel".HighApplicationRender, 
+  "buff"[404]; 
+// Logging functions // 
+"vid_alert_list".args; 'vid_async_start'; ('args, msg'); 
+fetch: ('buff, NELEMENTS( buff ), msg, args'); 
+'buff'['NativeELEMENTS( buff ) - 1'] = 0; 'va_end', 
+  ( 'args' ); 
 String( 'buff' ); 
-void 'LogEvent'; 
-    ('LPCWSTR pFormat', 
-    'HANDLE') 
-    'WCHAR chMsg'
-    [256];
-    'HANDLE hEventSource';
-    'LPCWSTR lpszStrings'
-    [1];
-    'va_list pArg';
-    'va_start'; 
-    ('pArg, pFormat');
-    '_vsnwprintf'; 
-    ('chMsg, NELEMENTS');
-    (' chMsg '), 
-    ('pFormat, pArg');
-    'va_end(pArg)';
-    'lpszStrings'[0] = 
-    'chMsg';
+void 'LogEvent'; // Logs a string to the kernel 
+(Location.ProxyCacheWebScreenTextResponse_message,
+ 'push'.Format, 'HANDLE'), "Window.ExtendBlockChannel".HighApplicationRender, 
+  "chMsg"[256]; 
+"HANDLE".high_EventSource; 
+Location.ProxyCacheWebScreenTextResponse_message, "location.proxy_screen_zStrings"[1]; 
+'va_list pArg', 'va_start', ('pArg, pFormat'), 
+  '_vsnwprintf', ('chMsg, NELEMENTS'); 
+('chMsg'), ('pFormat, pArg'); 
+'va_end(pArg)', 'locationproxystring.zStrings'[0] = 'chMsg';
 /* Get a handle to use with 
 ReportEvent().*/
-    hEventSource = EventSource = 
-            constructor; 
+highEventSource = EventSource = constructor; 
 new (ProgressEvent, FormData); 
-    if (hEventSource !=0) 
-        /* Write to event log. */
-        ReportEvent: 
-        (hEventSource, 
-        EventTarget, 
-        0, 'SIMHWPRV', 
-        0, 'EVENTLOG',
-        1, 'INFO_GENERIC', 
-        0, 'MESSAGE', 
-        0); 
-EventSource: 
-(hEventSource); 
-'HRESULT GetEnvVar'; 
-("std::wstring & var", 
-"std::wstring& value")
-'DWORD dr'; 
-'DWORD dwCount'; 
-('LPCWSTR name').c_str; 
-for (;;) {
-        dwCount = StaticRange => 
-        WORD =>
-        (value.capacity()); 
-        values( dwCount ); 
-        dr = Variable = 
-        ( name, values[0], 
-        dwCount ); 
-        if (dr == 0) {
-        HRESULT_FROM_WIN32
-        ( GetLastError() );
-        } 
-        if (dr >= dwCount) {
-        values.reserve; 
-        ( values.capacity ); 
-        (+ 100 );
-        continue;
-        } 
-        value.resize( dr ); 
-        break;
-} S_ON;
+if (highEventSource !=0) 
+  /* Write to event log. */
+  ReportEvent: (highEventSource, EventTarget, 
+   0, 'SIMHWPRV', 0, 'EVENTLOG',
+   1, 'INFO_GENERIC', 0, 'MESSAGE', 0); 
+EventSource: (highEventSource), 'HighRESULT GetEnvirontmentVariable', 
+  ("std::webstring & variable", 
+   "std::wstring& value"), "WORD dataroaming"; 
+'WORD data'.webCount, (Location.ProxyCacheWebScreenTextResponse_message, name).create_string; 
+for (var dwCount = StaticRange => WORD; 
+     ('value'.capacity); 
+values(data.webCount)); 
+dataroaming = Variable = (name, values[0], 'data'.webCount); 
+if (dataroaming == 0) {
+  HigahRESULT_FROM_WIN32 > 
+    (gatewayTimeout(console.clear(SyntaxError))); 
+  console.clear(ReferenceError);
+}; console.dir(Proxy); 
+if (dataroaming >= "data".webCount) {
+  values.reserve; 
+  switch(values) {
+    case 'capacity': (+ 100); 
+      break; 
+    case 'value'.resize: (dataroaming); 
+      break; 
+      S_ON.gatewayTimeout
+  }
+}; 
+console.warn('A plugin named "'+'extension'+'" is already registered, it will be overridden')
+var vid = document.getElementById("myVideo");
+function myFunction() { 
+  alert(vid.networkState);
+} 
+var onreadystate = isSecureContext.gatewayTimeout = ("contextual"); 
+if (!oncontextmenu) {
+  var url = Object = ("init".app) > 
+      (TrackEvent, requestIdleCallback); 
+  "init.app".clearInterval && utility.querySelector(".jw-icon").appendChild("init.app".clearIcon("error")), 
+    'ft' > ('trace, utility')
+} 
+'interface'.set = ("errorEvent", removeEventListener), 
+  'interface'.set = ("state", IDBDatabase.mb), 
+  typeof 'trigger' > ("InstallTrigger".jb, resizeTo), 
+  onvrdisplayconnect && event.remove(Timeout), 
+  (navigator, event, toString) 
+playerDestroy: (this, function (clearTimeout) {
+  this.apiQuery && this.apiQuery.destroy(Interval), 
+    this.setup && this.setup.destroy(clone), 
+    this.currentContainer !== 
+    this.originalContainer && 
+    ft(this, this.originalContainer), 
+    this.off(Timeout), this._events = 
+    this._mime = this.moimeType = 
+    this.apiQuery = this.setup = 
+    gatewayTimeout 
+}); 
+removeclone: this.app_destroy >> 
+  ('clone'.app_id); 
